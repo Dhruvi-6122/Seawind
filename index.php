fb6d1186c60ae0c1dadf6bcf3ca2c42cd68ef167
Dhruvi-6122 <112571116+Dhruvi-6122@users.noreply.github.com>
2023-11-05 22:17:10 +0530

    FirstTry
    Almost Done

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- Main Page -->
+<section class="banner">
+    <div class="banner-overlay">
+        <header class="header">
+            <div class="container">
+                <div class="left">
+                    LOGO
+                </div>
+                <div class="middle">
+                    <ul>
+                        <a href="/"><li>Home</li></a>
+                        <a href="/"><li>Models</li></a>
+                        <a href="/"><li>Register As a Model</li></a>
+                        <a href="/"><li>Hire As a Model</li></a>
+                        <a href="/"><li>Services</li></a>
+                        <a href="/"><li>About us</li></a>
+                        <a href="/"><li>Contact us</li></a>
+                    </ul>
+                </div>
+                <div class="right">
+                
+                    <button class="btn" id="button1">Login</button>
+                    <button class="btn" id="button2">Register</button>
+                
+                </div>
+            </div>
+        </header>
+    </div>
+</section>
+
+<!-- Popular Models -->
+<section class="PopularModels">
+    <div class="Heading">
+        <h1 class="text">Popular Models</h1>
+    </div>
+    <div class="SecondContainer">
+        <div class="item" >
+            <img class="img" src="./Image/m1.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m2.jpeg" alt="">
+            <h3 class="FirstText">PETER</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m3.jpeg" alt="">
+            <h3 class="FirstText">BEATRIZ</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m4.jpeg" alt="">
+            <h3 class="FirstText">ALEXIS RANE</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m5.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m6.jpeg" alt="">
+            <h3 class="FirstText">LAURI</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m7.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m8.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m9.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m10.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m11.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m12.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m13.jpeg" alt="">
+            <h3 class="FirstText">ERICA</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m14.jpeg" alt="">
+            <h3 class="FirstText">PAMELA STONEM</h3>
+        </div>
+        <div class="item" >
+            <img class="img" src="./Image/m15.jpeg" alt="">
+            <h3 class="FirstText">BARBARA SERNEY</h3>
+        </div>
+        
+    </div>
+    
+</section>
+
+<!-- Become a model -->
+<section class="RegisterModel">
+    <div class="Register">
+        <h1 class="Model">BECOME A MODEL</h1>
+        <button class="btn1">APPLY NOW</button>
+    </div>
+</section>
+
+<!-- Model categories -->
+<section class="HireModel">
+    <div class="Heading">
+        <h1 class="text">Model Categories</h1>
+    </div>
+    <div class="Container4">
+      <div class="Con1">
+        <div class="boxModel" id="boxModel1">
+            <h3 class="SmallHead">FEMALE MODELS</h3>
+            <button class="Sbtn">EXPLORE PROFILE</button>
+            <div>
+                <img class="Simg" src="./Image/femalemodel.jpg" height="550px" width="400px" >
+            </div>
+        </div>
+        <div class="boxModel" id="boxModel2">
+            <h3 class="SmallHead">MALE MODELS</h3>
+            <button class="Sbtn">EXPLORE PROFILE</button>
+            <div>
+                <img class="Simg" src="./Image/malemodel.jpeg" height="550px" width="400px">
+            </div>
+        </div>
+      </div>
+      <div class="Con2">
+        <div class="boxModel" id="boxModel3">
+            <h3 class="SmallHead">CELEBRITIES</h3>
+            <button class="Sbtn">EXPLORE PROFILE</button>
+            <div>
+                <img class="Simg" src="./Image/celebrities.jpeg" height="550px" width="400px">
+            </div>
+        </div>
+        <div class="boxModel" id="boxModel4">
+            <h3 class="SmallHead">KIDS MODELS</h3>
+            <button class="Sbtn">EXPLORE PROFILE</button>
+            <div>
+                <img class="Simg" src="./Image/kidsmodels.jpeg" height="550px" width="400px">
+            </div>
+        </div>
+
+      </div>
+    </div>
+</section>
+
+<!-- Services -->
+<section class="Services">
+    <div class="Heading">
+        <h1 class="text">Our Services</h1>
+    </div>
+    
+    <div class="Container5">
+        <div class="box" id="box1">PROMOTIONAL MODELS</div>
+        <div class="box" id="box2">BRAND AMBASSADORS</div>
+        <div class="box">EVENT MANAGEMENT</div>
+        <div class="box">STUDIO RENTALS</div>
+        <div class="box">PHOTOGRAPHY MODELS</div>
+        <div class="box">RUNWAY MODELS</div>
+        <div class="box" id="box7">TELEVISION MODELS</div>
+        <div class="box" id="box8">MARKETING</div>
+    </div>
+</section>
+
+<!-- About us -->
+<section class="Section6">
+    <div class="Heading">
+        <h1 class="text">About Us</h1>
+    </div>
+    <div class="AboutUs">
+        <div class="About">The point of this article is to help you grasp the big picture of getting six
+            pack abs. In short, to get six pack abs, you need to get leaner and develop your abs,
+            musculature. This is done through training and nutrition. The topic of nutrition is 
+            covered extensively elsewhere on the net: I won't delve in it. Training-wise , to build 
+            six pack abs, you must combine cardio exercise, weightlifting, and abdominal exercises.
+            All three are important, and to really get results you must do all three.
+            <br>
+            <br>
+            Cardio makes you leaner,which is necessary if you want your abs to show. weightlifting
+            makes you leaner,too, by increasing your resting metabolic rate and the amount of 
+            calories you burn at rest. Good weightlifting routines for fat loss are also available 
+            elsewhere online. Finally abdomonal exercises are needed to define your abs and give 
+            them the six pack look. 
+        </div>
+    
+        <div class="AboutMore">
+            <div class="div" id="div1">Over 20 yesr experience</div>
+            <div class="div" id="div2">Professional advice</div>
+            <div class="div" id="div3">The best model agency in the world</div>
+        </div>
+    </div>
+</section>
+
+<section class="ContactUs"></section>
+
+
+
+
+<!-- <script>
+    var btn = document.getElementById('button1');
+    btn.addEventListener('click', function(){
+        document.location.href = 'login.php';
+    })
+</script> -->
+</body>
+</html>
